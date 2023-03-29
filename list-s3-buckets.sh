d0c0a0b833ccf511119b25fb199f9876f666dae8
pize338 <89057554+pize338@users.noreply.github.com>
2023-03-29 14:23:08 +0700

    Create list-s3-buckets.sh
    Ref aws cli
    
    >> https://docs.aws.amazon.com/cli/latest/reference/s3/ls.html

---
+++ list-s3-buckets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: list-s3-buckets.sh AWSprofileName "
+  echo "./list-s3-buckets.sh AWSprofileName"
+  exit 1
+fi
+
+AWS_PROFILE="$1"
+output_file="buckets-$AWS_PROFILE.csv"
+
+# Get bucket list and write output to buckets-$AWS_PROFILE.csv
+echo "BucketName,CreatedDate" > "$output_file"
+
+aws s3api list-buckets \
+--query 'Buckets[*].[Name,CreationDate]' \
+--output json \
+--profile "$AWS_PROFILE" | jq -r '.[] | [.[] | tostring] | @csv' | while read -r row; do
+
+  # Write the output to the file
+  echo "$row" >> "$output_file"
+
+  # Print a message indicating the results were written to the file
+  echo "Bucket $BUCKET_NAME written to $output_file"
+done
+
+# Print a final message indicating the script has completed
+echo "Script completed successfully. Output written to $output_file"
