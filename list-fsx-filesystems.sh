cf8804b552965737036c4d0706c50fbc92435dd3
pize338 <89057554+pize338@users.noreply.github.com>
2023-03-30 14:23:12 +0700

    Create list-fsx-filesystems.sh
    https://docs.aws.amazon.com/cli/latest/reference/fsx/describe-file-systems.html

---
+++ list-fsx-filesystems.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: list-fsx-filesystems.sh AWSprofileName "
+  echo "./list-fsx-filesystems.sh AWSprofileName"
+  exit 1
+fi
+
+AWS_PROFILE="$1"
+output_file="filesystems-$AWS_PROFILE.csv"
+
+# Get filesystem list and write output to filesystems-$AWS_PROFILE.csv
+echo "FileSystemId,FileSystemType,DeploymentType,StorageType,StorageCapacityGiB" > "$output_file"
+
+aws fsx describe-file-systems \
+--query 'FileSystems[*].[FileSystemId,FileSystemType,WindowsConfiguration.DeploymentType,StorageType,StorageCapacity]' \
+--output json \
+--profile "$AWS_PROFILE" | jq -r '.[] | @csv' | while read -r row; do
+
+  # Write the output to the file
+  echo "$row" >> "$output_file"
+
+  # Print a message indicating the results were written to the file
+  #FILESYSTEM_ID=$(echo "$row" | cut -d',' -f1)
+  #echo "FileSystem $FILESYSTEM_ID written to $output_file"
+done
+
+# Print a final message indicating the script has completed
+echo "Script completed successfully. Output written to $output_file"
